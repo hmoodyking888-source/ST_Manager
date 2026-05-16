82b0214ea796d32c489e703becf512920e774da8
hmoodyking888-source <hmoodyking888@gmail.com>
2026-05-16 04:59:00 +0300

    Update Android plugin version to 8.11.1 to match project classpath

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,9 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
+    // تم تحديث الإصدار هنا إلى 8.11.1 ليطابق مشروعك تماماً
+    id("com.android.application") version "8.11.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-    // إضافة الفايربيس بالشكل الصحيح المتوافق مع Kotlin DSL
+    // إضافة الفايربيس المتوافقة
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 

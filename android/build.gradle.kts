8ca9e43b2b0d5f83d58695b5a728ac571f504d38
hmoodyking888-source <hmoodyking888@gmail.com>
2026-05-16 05:04:42 +0300

    Fix Kotlin plugin version to 2.2.20 to match project constraints

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    // تم تحديث الإصدار هنا إلى 8.11.1 ليطابق مشروعك تماماً
+    // إصدار الأندرويد المطابق لمشروعك
     id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    // تم تحديث إصدار كوتلن هنا إلى 2.2.20 ليطابق مشروعك تماماً وتنتهي المشكلة
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
-    // إضافة الفايربيس المتوافقة
+    // إضافة الفايربيس المستقرة
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 

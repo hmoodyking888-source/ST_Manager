4b0120e91528df1e542ef43058ec8739f1d8357f
hmoodyking888-source <hmoodyking888@gmail.com>
2026-05-16 05:10:20 +0300

    Remove hardcoded flutter gradle plugin version to avoid conflict

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,9 +2,10 @@
 plugins {
     // إصدار الأندرويد المطابق لمشروعك
     id("com.android.application") version "8.11.1" apply false
-    // تم تحديث إصدار كوتلن هنا إلى 2.2.20 ليطابق مشروعك تماماً وتنتهي المشكلة
+    // إصدار كوتلن المطابق لمشروعك
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    // تم حذف تحديد الإصدار هنا ليقوم فلاتر بإدارة نفسه تلقائياً ومنع التضارب
+    id("dev.flutter.flutter-gradle-plugin") apply false
     // إضافة الفايربيس المستقرة
     id("com.google.gms.google-services") version "4.4.2" apply false
 }

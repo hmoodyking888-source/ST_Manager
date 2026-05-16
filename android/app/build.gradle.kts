87bb2c7b7499561d0b3d62681e0a7c2358628060
hmoodyking888-source <hmoodyking888@gmail.com>
2026-05-16 04:53:55 +0300

    Replace both gradle files with correct Firebase Kotlin DSL syntax

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,29 +1,29 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // تفعيل الفايربيس هنا في نظام الأندرويد الجديد
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.st_manager"
+    namespace = "com.example.st_manager" // تأكد أن هذا يطابق اسم حزمة تطبيقك الأصلي إن كان مختلفاً
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html)
         applicationId = "com.example.st_manager"
         // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        // For more information, see: https://flutter.dev/to/review-gradle-config
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,12 +33,12 @@ android {
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now, so flutter run --release works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
-apply plugin: 'com.google.gms.google-services'
+
 flutter {
     source = "../.."
 }
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,26 +1,12 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    // إضافة الفايربيس بالشكل الصحيح المتوافق مع Kotlin DSL
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-
-classpath 'com.google.gms:google-services:4.4.2'
+    delete(rootProject.buildDir)
 }
